7a3f8c6dac5e73e0f18d320a43d2e21eae289a89
akash <sdeveloperakash@gmail.com>
2025-06-12 00:10:07 +0530

    eureka -up

--- eureka-server/src/main/java/com/service/eureka_server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/service/eureka_server/EurekaServerApplication.java
@@ -1,9 +1,11 @@
-package com.serve.eureka_server;
+package com.service.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
--- eureka-server/src/test/java/com/service/eureka_server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/service/eureka_server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.serve.eureka_server;
+package com.service.eureka_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
