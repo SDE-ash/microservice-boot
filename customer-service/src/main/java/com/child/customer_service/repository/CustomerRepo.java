8cfad1c9de23e4620f56575fdfd13065491a1b45
akash <sdeveloperakash@gmail.com>
2025-06-12 00:22:23 +0530

    removed unwanted imports form controller

--- customer-service/src/main/java/com/child/customer_service/repository/CustomerRepo.java
+++ customer-service/src/main/java/com/child/customer_service/repository/CustomerRepo.java
@@ -4,7 +4,7 @@ import com.child.customer_service.modal.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Integer> {
--- customer-service/src/main/java/com/child/customer_service/services/CustomerService.java
+++ customer-service/src/main/java/com/child/customer_service/services/CustomerService.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Optional;
+
 
 @Service
 public class CustomerService {
